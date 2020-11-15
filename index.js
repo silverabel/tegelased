16c213237707e46ea6e3ee0545b3579bbdbdb8b1
Silver Abel <silverabel66@gmail.com>
2020-11-15 20:13:13 +0200

    reformat, added async prompt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
+let firebaseConfig = {
   apiKey: "AIzaSyAjWtpKAuv0yF1lLr5WHJKADMyD0XyzIqY",
   authDomain: "tegelased-7951a.firebaseapp.com",
   databaseURL: "https://tegelased-7951a.firebaseio.com",
@@ -48,71 +48,87 @@ class Tegelane {
     this.joonistaJalad();
 
     text(this.nimi, this.x, this.y - 15);
+    text(ajadSurmani[this.nimi], this.x, this.y - 25)
   }
 }
 
 
-let minuTegelane;
-let minuTegelaseRef;
-
-function init() {
-  let minuTegelaseNimi = prompt("Sisesta nimi");
-  if (!minuTegelaseNimi) minuTegelaseNimi = "ei taha nime panna" + Math.floor(Math.random() * 100);
-  minuTegelane = new Tegelane(minuTegelaseNimi, 640, 360, "white");
-  minuTegelaseRef = firebase.database().ref(`tegelased/${minuTegelaseNimi}`);
-};
-document.addEventListener("DOMContentLoaded", init);
-
-
-let tegelasedMassiiv = [];
-let ajadSurmaniMassiiv = [];
-let eluAegKaadrites = 3600;
+let tegelasedMap = new Map();
+let ajadSurmani = {};
+let eluAegKaadrites = 600;
 let tegelasedRef = firebase.database().ref("tegelased");
-
 tegelasedRef.on("child_added", (snapshot) => {
   if (snapshot.key == minuTegelane.nimi) return;
-  tegelasedMassiiv[snapshot.key] = new Tegelane(snapshot.key, snapshot.val().x, snapshot.val().y, snapshot.val().värv);
-  ajadSurmaniMassiiv[snapshot.key] = eluAegKaadrites;
+  tegelasedMap.set(snapshot.key, new Tegelane(snapshot.key, snapshot.val().x, snapshot.val().y, snapshot.val().värv));
+  ajadSurmani[snapshot.key] = eluAegKaadrites;
   
-  tegelasedRef.child(snapshot.key).on("value", onValueChanged);
+  tegelasedRef.child(snapshot.key).on("value", onChildValueChanged);
 });
 
-function onValueChanged(snapshot) {
-  if (!snapshot.val()) return;
-  tegelasedMassiiv[snapshot.key].uusX = snapshot.val().x;
-  tegelasedMassiiv[snapshot.key].uusY = snapshot.val().y;
-  tegelasedMassiiv[snapshot.key].värv = snapshot.val().värv;
+function onChildValueChanged(snapshot) {
+  let tegelane = tegelasedMap.get(snapshot.key);
+  tegelane.uusX = snapshot.val().x;
+  tegelane.uusY = snapshot.val().y;
+  tegelane.värv = snapshot.val().värv;
 }
 
-
 tegelasedRef.on("child_removed", (snapshot) => {
-  delete tegelasedMassiiv[snapshot.key];
-  delete ajadSurmaniMassiiv[snapshot.key];
-  tegelasedRef.child(snapshot.key).off();
+  tegelasedMap.delete(snapshot.key);
+  delete ajadSurmani[snapshot.key];
 });
 
 
-
+let minuTegelane;
 let canvas;
 let värvideVorm;
+let p5Prompt;
 function setup() {
   canvas = createCanvas(1280, 720);
   background("gray");
   textAlign(CENTER);
-  frameRate(50);
-
-  document.värvideVorm.hidden = false;
+  
   värvideVorm = new p5.Element(document.värvideVorm);
+  värvideVorm.position(canvas.position().x + 1100, canvas.position().y + 30);
+  document.värvideVorm.hidden = false;
+
+  minuTegelane = new Tegelane("Nimetu", 640, 360, "white");
+
+  let prompt = new PromptDialog();
+  p5Prompt = new p5.Element(prompt.el);
+  p5Prompt.position(canvas.position().x + 540, canvas.position().y + 200);
+
+  prompt.show('Sisesta nimi')
+    .waitForUser()
+    .then(function(nimi) {
+      document.getElementsByTagName("body")[0].focus();
+      minuTegelane.nimi = nimi || "ei taha nime panna" + Math.floor(Math.random() * 100);
+      tegelasedRef.child(minuTegelane.nimi).set({
+        x: minuTegelane.x,
+        y: minuTegelane.y,
+        värv: minuTegelane.värv
+      });
+    })
+    .catch(function(error) {
+      console.log("Error: ", error);
+    })
+    .finally(function() {
+      prompt.hide();
+    });
 }
+
+
+function windowResized() {
+  värvideVorm.position(canvas.position().x + 1100, canvas.position().y + 30);
+  p5Prompt.position(canvas.position().x + 540, canvas.position().y + 200);
+}
+
 
 let freeToSend = true;
 function draw() {
-  let keyDown = false;
-
   background("gray");
-  
-  värvideVorm.position(canvas.position().x + 1100, canvas.position().y + 30);
 
+  let keyDown = false;
+  
   if (keyIsDown(LEFT_ARROW)) {
     minuTegelane.x -= 5;
     keyDown = true;
@@ -132,7 +148,7 @@ function draw() {
 
   if (keyDown && freeToSend) {
     freeToSend = false;
-    minuTegelaseRef.set({
+    tegelasedRef.child(minuTegelane.nimi).set({
       x: minuTegelane.x,
       y: minuTegelane.y,
       värv: minuTegelane.värv
@@ -142,38 +158,41 @@ function draw() {
 
   minuTegelane.joonista();
 
-  for (tegelaseNimi in tegelasedMassiiv) {
+  
+  for (let [tegelaseNimi, tegelane] of tegelasedMap) {
 
-    if (ajadSurmaniMassiiv[tegelaseNimi] < 1) {
-      firebase.database().ref(`tegelased/${tegelaseNimi}`).remove();
+    if (ajadSurmani[tegelaseNimi] < 1) {
+      tegelasedRef.child(tegelaseNimi).off();
+      tegelasedRef.child(tegelaseNimi).remove();
       continue;
     };
 
+    let x = tegelane.x;
+    let y = tegelane.y;
+    let uusX = tegelane.uusX;
+    let uusY = tegelane.uusY;
 
-    let x = tegelasedMassiiv[tegelaseNimi].x;
-    let y = tegelasedMassiiv[tegelaseNimi].y;
-    let uusX = tegelasedMassiiv[tegelaseNimi].uusX;
-    let uusY = tegelasedMassiiv[tegelaseNimi].uusY;
-
-    if (x != uusX || y != uusY) ajadSurmaniMassiiv[tegelaseNimi] = eluAegKaadrites;
-    else ajadSurmaniMassiiv[tegelaseNimi] -= 1;
+    if (x != uusX || y != uusY) ajadSurmani[tegelaseNimi] = eluAegKaadrites;
+    else ajadSurmani[tegelaseNimi] -= 1;
 
     
-    if (uusX - x >= 5) tegelasedMassiiv[tegelaseNimi].x += 5;
-    else if (uusX - x <= -5) tegelasedMassiiv[tegelaseNimi].x -= 5;
-    else tegelasedMassiiv[tegelaseNimi].x = uusX;
+    if (uusX - x >= 5) tegelane.x += 5;
+    else if (uusX - x <= -5) tegelane.x -= 5;
+    else tegelane.x = uusX;
 
-    if (uusY - y >= 5) tegelasedMassiiv[tegelaseNimi].y += 5;
-    else if (uusY - y <= -5) tegelasedMassiiv[tegelaseNimi].y -= 5;
-    else tegelasedMassiiv[tegelaseNimi].y = uusY;
+    if (uusY - y >= 5) tegelane.y += 5;
+    else if (uusY - y <= -5) tegelane.y -= 5;
+    else tegelane.y = uusY;
     
-    tegelasedMassiiv[tegelaseNimi].joonista();
+    tegelane.joonista();
   }
 }
 
 
 function keyReleased() {
-  minuTegelaseRef.set({
+  if (![LEFT_ARROW, RIGHT_ARROW, UP_ARROW, DOWN_ARROW].includes(keyCode)) return;
+  
+  tegelasedRef.child(minuTegelane.nimi).update({
     x: minuTegelane.x,
     y: minuTegelane.y,
     värv: minuTegelane.värv
@@ -182,9 +201,75 @@ function keyReleased() {
 
 
 var värviNupud = document.getElementsByName("värvid");
-värviNupud.forEach (värviNupp => värviNupp.style.backgroundColor = värviNupp.value);
-
-document.värvideVorm.onclick = function() {
-  var checkedVärviNuppElement = document.querySelector('input[name = värvid]:checked')
-  minuTegelane.värv = checkedVärviNuppElement.value;
+for (let värviNupp of värviNupud) {
+  värviNupp.style.backgroundColor = värviNupp.value
+  värviNupp.onclick = function() {
+    minuTegelane.värv = this.value;
+  }
+  värviNupp.onmousedown = function() {
+    this.onmouseup = function() {
+      tegelasedRef.child(minuTegelane.nimi).update({
+        värv: this.value
+      })
+      this.onmouseup = null;
+    }
+  }
 }
+
+
+
+
+var noop = function() {
+  return this;
+};
+
+function Dialog() {
+  this.setCallbacks(noop, noop);
+}
+Dialog.prototype.setCallbacks = function(okCallback, cancelCallback) {
+  this._okCallback     = okCallback;
+  this._cancelCallback = cancelCallback;
+  return this;
+};
+Dialog.prototype.waitForUser = function() {
+  var _this = this;
+  return new Promise(function(resolve, reject) {
+    _this.setCallbacks(resolve, reject);
+  });
+};
+Dialog.prototype.show = noop;
+Dialog.prototype.hide = noop;
+
+
+function PromptDialog() {
+  Dialog.call(this);
+  this.el           = document.getElementById('dialog');
+  this.inputEl      = this.el.querySelector('input');
+  this.messageEl    = this.el.querySelector('.message');
+  this.okButton     = this.el.querySelector('button.ok');
+  this.cancelButton = this.el.querySelector('button.cancel');
+  this.attachDomEvents();
+}
+PromptDialog.prototype = Object.create(Dialog.prototype);
+PromptDialog.prototype.attachDomEvents = function() {
+  var _this = this;
+  this.okButton.addEventListener('click', function() {
+    _this._okCallback(_this.inputEl.value);
+  });
+  this.cancelButton.addEventListener('click', function() {
+    _this._cancelCallback();
+  });
+  this.inputEl.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") _this._okCallback(_this.inputEl.value);
+  });
+};
+PromptDialog.prototype.show = function(message) {
+  this.messageEl.innerHTML = String(message);
+  this.el.className = '';
+  this.inputEl.focus();
+  return this;
+};
+PromptDialog.prototype.hide = function() {
+  this.el.className = 'hidden';
+  return this;
+};
